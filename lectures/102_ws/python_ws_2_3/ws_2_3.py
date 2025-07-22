60cbf624f69a03fd6da3922490ecaf6befcbc064
jane <dlffldk8060@naver.com>
2025-07-22 22:29:48 +0900

    실습과제3

--- lectures/102_ws/python_ws_2_3/ws_2_3.py
+++ lectures/102_ws/python_ws_2_3/ws_2_3.py
@@ -5,14 +5,6 @@
 # MovieTheater 클래스를 상속받는 VIPMovieTheater 클래스를 정의한다.
     # VIPMovieTheater 클래스는 VIP 좌석 수를 저장하는 vip_seats 인스턴스 변수를 가진다.
     # 생성자에서 처리되어야 한다.
-# VIPMovieTheater 클래스는 VIP 좌석을 예약하는 reserve_vip_seat 메서드를 가진다.
-    # reserve_vip_seat 메서드는 예약 가능한 VIP 좌석이 있는 경우, vip_seats를 1 감소시키고 예약 성공 메시지를 반환한다.
-    # 예약 가능한 VIP 좌석이 없는 경우, 예약 실패 메시지를 반환한다.
-# VIPMovieTheater 클래스는 reserve_seat 메서드를 오버라이딩하여, VIP 좌석이 먼저 예약되도록 한다.
-# VIP 좌석이 예약 가능한 경우, reserve_vip_seat 메서드를 호출하여 VIP 좌석을 예약한다.
-# VIP 좌석이 예약 불가능한 경우, 부모 클래스의 reserve_seat 메서드를 호출하여 일반 좌석을 예약한다.
-
-
 
 class MovieTheater :
     def __init__(self,name,total_seats):
@@ -35,28 +27,36 @@ class MovieTheater :
         print(f"예약된 좌석 수: {self.reserved_seats}")
 
 
+# VIPMovieTheater 클래스는 VIP 좌석을 예약하는 reserve_vip_seat 메서드를 가진다.
+    # reserve_vip_seat 메서드는 예약 가능한 VIP 좌석이 있는 경우, vip_seats를 1 감소시키고 예약 성공 메시지를 반환한다.
+    # 예약 가능한 VIP 좌석이 없는 경우, 예약 실패 메시지를 반환한다.
+# VIPMovieTheater 클래스는 reserve_seat 메서드를 오버라이딩하여, VIP 좌석이 먼저 예약되도록 한다.
+# VIP 좌석이 예약 가능한 경우, reserve_vip_seat 메서드를 호출하여 VIP 좌석을 예약한다.
+# VIP 좌석이 예약 불가능한 경우, 부모 클래스의 reserve_seat 메서드를 호출하여 일반 좌석을 예약한다.
+
+
 class VIPMovieTheater(MovieTheater) :
-    def __init__(self) :
-        super().__init__
-        self.vip_seats = 20
+    def __init__(self,name,total_seats,vip_seats) :
+        super().__init__(name,total_seats)
+        self.vip_seats = vip_seats
 
     def reserve_vip_seat(self) :
-        if self.reserve_vip_seat > 0 :
+        if self.vip_seats > 0 :
             self.vip_seats -= 1
+            self.reserved_seats += 1
             print("VIP 좌석 예약이 완료되었습니다.")
         else : 
-            super().reserve_seat()
+            print("예약 가능한 VIP 좌석이 없습니다.")
+
     def reserve_seat(self) :
-        if self.reserved_seats < self.total_seats :
-            self.reserved_seats += 1
-            print("좌석 예약이 완료되었습니다.")
-        else : 
-            print("예약이 가능한 좌석이 없습니다.")
+        if self.vip_seats > 0 :
+            self.reserve_vip_seat()
+        else:
+            super().reserve_seat()
 
-mt1 = MovieTheater('메가박스',50)
-mt2 = MovieTheater('CGV', 120)
-mt1.reserve_seat()
-mt1.reserve_seat()
-mt1.reserve_seat()
-
-mt1.current_status()
+vp1 = VIPMovieTheater('메가박스',4,3)
+vp1.reserve_seat()
+vp1.reserve_seat()
+vp1.reserve_seat()
+vp1.reserve_seat()
+vp1.reserve_seat()
